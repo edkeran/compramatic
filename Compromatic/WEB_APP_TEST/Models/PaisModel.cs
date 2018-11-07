1b817eac5dec07dc75bd14949a684f4c09c7c65d
Edgar Andres Krejci <Edgar Andres Krejci@LAPTOP-4IS4IJVU>
2018-11-06 19:47:18 -0500

    UPDATE WEB_APP

---
+++ Compromatic/WEB_APP_TEST/Models/PaisModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_APP_TEST.Models
+{
+    public class PaisModel
+    {
+    }
+}
