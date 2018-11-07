dd9d6aae0e6a73a379829601a99a9539c218df66
pc <pc@DESKTOP-6445IKD>
2018-11-06 23:28:35 -0500

    LECTURA DATOS MVC

---
+++ Compromatic/MVC5/Controllers/AlumnoController.cs
@@ -0,0 +1,31 @@
+﻿using MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            AlumnosContext db = new AlumnosContext();
+            //List<Alumno> lista= db.Alumno.Where(a=>a.Edad>18).ToList();
+            return View(db.Alumno.ToList());
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
+        {
+            return View();
+        }
+    }
+}
